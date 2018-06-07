44713c683e6752d713e7d6fc4fc50bbf9ba1c161
fyh pc <aszx365289461@github.com>
2018-06-08 07:58:01 +0800

    express

--- js练习/promise.js
+++ js练习/promise.js
@@ -16,3 +16,30 @@ Promise.all([p,p2,p1]).then((res)=>{
 }).catch(error=>{
     console.log('catch',error);
 });
+
+
+Vue.prototype.keyQuery = function (keyword, queryTyp) {
+    let dataJson = {
+      userId: 'b77e3b75-7dde-4704-bc98-7ac698052e08',
+      scenicAreaId: localStorage.getItem('scenicAreaIdStorage'),
+      queryTyp: queryTyp,
+      keyword: keyword,
+    }
+    return this.$http({
+      method: "get",
+      url: "/feapi/globalInfo/keyQuery",
+      params: dataJson,
+      dataType: "json",
+      crossDomain: true,
+      cache: false,
+    }).then(resolve => {
+      var keyQueryStorage = resolve.data.result;
+      keyQueryStorage = JSON.stringify(keyQueryStorage); //转化为JSON字符串
+      localStorage.setItem("keyQueryStorage", keyQueryStorage);
+
+      console.log('我在请求', JSON.parse(localStorage.getItem("keyQueryStorage")))
+      return Promise.resolve(1111);
+    }), reject => {
+      return Promise.reject(error)
+    };
+  };
