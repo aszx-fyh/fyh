6d18b9c7d66ace0f56e43576e50628ad2259fade
aszx-fyh <365289462@qq.com>
2018-06-25 17:55:09 +0800

    'add'

--- vue-demo/backend/app.js
+++ vue-demo/backend/app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
-
+app.use(express.static('./static'));
 app.get('/', function (req, res) {
   res.setHeader('Access-Control-Allow-Origin','*');
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000,'192.168.1.131' ,function () {
   var host = server.address().address;
   var port = server.address().port;
 
